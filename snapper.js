06afbd018b2fc4eab32f73b2ff9d60047f439c6f
Boris Cherny <bcherny@gmail.com>
2014-03-16 22:41:18 -0700

    progress..?

--- snapper.js
+++ snapper.js
@@ -3,6 +3,7 @@
 var fs = require('fs')
   , phantom = require('phantom')
   , options = JSON.parse(fs.readFileSync('./config.json'))
+  , url = 'http://console.turn.com'
   ;
 
 
@@ -11,27 +12,36 @@ phantom.create(function (ph) {
 
 		var n = 0
 		  , interval
+		  , path = ['.', options.folder, url.slice(url.lastIndexOf('/'))].join('/')
 		  ;
 
-		page.clipRect = options.viewport;
+		page.set('onLoadStarted', function() {
 
-		page.open('http://console.turn.com/', function (status) {
-			clearInterval(interval);
+			interval = setInterval(function() {
+
+				var time = options.interval * n
+				  , file = path + '/' + time + '.png'
+				  ;
+
+				page.render(file, function (file) {
+
+					console.log('rendered ' + file + '!');
+
+				}.bind(null, file));
+
+				console.log('snapping at ' + time + 'ms -> ' + file);
+
+				++n;
+
+			}, options.interval);
+
+		});
+
+		// open page
+		page.open(url, function (status) {
+			setTimeout(clearInterval.bind(null, interval), options.interval);
 			ph.exit();
 		});
 
-		interval = setInterval(function() {
-
-			var time = options.interval * n
-			  , file = './' + options.folder + '/' + time + '.png'
-			  ;
-
-			page.render(file);
-
-			console.log('snapping at ' + time + 'ms -> ' + file)
-
-			++n;
-
-		}, options.interval);
 	});
 });
