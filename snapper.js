c7b5ea95796a9e86d3baab85b82b7a0802b35a52
Boris Cherny <bcherny@gmail.com>
2014-03-19 22:11:40 -0700

    stable, but buggy

--- snapper.js
+++ snapper.js
@@ -3,9 +3,8 @@
 var fs = require('fs')
   , phantom = require('phantom')
   , options = JSON.parse(fs.readFileSync('./config.json'))
-  , url = 'http://console.turn.com'
+  , url = process.argv[2]
   ;
-
 
 phantom.create(function (ph) {
 	ph.createPage(function (page) {
@@ -21,16 +20,14 @@ phantom.create(function (ph) {
 			interval = setInterval(function() {
 
 				var time = options.interval * n
-				  , file = path + '/' + time + '.pdf'
+				  , file = path + '/' + time + '.' + options.format
 				  ;
 
-				page.renderBase(file, function (data64) {
+				page.render(file, function () {
 
-					snaps[file] = data64;
+					console.log('snapping at ' + time + 'ms -> ' + file);
 
 				});
-
-				console.log('snapping at ' + time + 'ms -> ' + file);
 
 				++n;
 
@@ -39,26 +36,13 @@ phantom.create(function (ph) {
 		});
 
 		// open page
-		page.open(url, function (status) {
+		page.open(url, setTimeout(function (status) {
 
 			// stop snapping
 			clearInterval(interval);
 
-			// save snaps to disk
-			// setTimeout(Object.keys(snaps).forEach(function (file) {
-
-			// 	fs.writeFile(file, new Buffer(snaps[file], 'base64'), function (err) {
-
-			// 		if (err) { throw new error (err); }
-
-			// 		else { console.log('saved ' + file); }
-
-			// 	});
-
-			// }), options.interval);
-
 			ph.exit();
-		});
+		}, 2000));
 
 	});
 });
